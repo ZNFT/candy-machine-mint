b6667bc2d8bd44c4dad17c0c781ebfab950296a2
Eric Kao <eric.kao@enjoy.com>
2021-10-03 00:56:00 -0700

    [MOD] Add rarity updates and margin to old css

--- src/pages/RarityTool.tsx
+++ src/pages/RarityTool.tsx
@@ -1,8 +1,14 @@
-import { Position, Spinner, SpinnerSize, Toaster } from "@blueprintjs/core";
+import {
+  Icon,
+  IconSize,
+  Position,
+  Spinner,
+  SpinnerSize,
+  Toaster,
+} from "@blueprintjs/core";
 import axios from "axios";
 import { debounce, find, get } from "lodash";
 import { useCallback, useEffect, useState } from "react";
-import AttributeList from "../components/AttributeList";
 import Header from "../components/Header";
 import { rarityHashMap, RarityTypes } from "../utils/attribute-hash";
 import { botsJson } from "../utils/bots";
@@ -40,23 +46,21 @@ const getRarityString = (
   traitName: string | undefined
 ) => {
   if (
-    (rarity === "none" && category === "Damage") ||
+    (category === "Equipment" && traitName === "none") ||
+    rarity === RarityTypes.COMMON
+  ) {
+    return "COMMON";
+  } else if (
+    (traitName === "none" && category === "Damage") ||
     rarity === RarityTypes.EPIC
   ) {
     return "PRETTY EPIC";
   } else if (rarity === RarityTypes.RARE) {
     return "LOOKS RARE";
-  } else if (
-    rarity === RarityTypes.COMMON ||
-    (category === "Equipment" && traitName === "None")
-  ) {
-    return "COMMON";
-  } else if (
-    rarity === "N/A" ||
-    rarity === RarityTypes.LEGENDARY ||
-    rarity === RarityTypes.UNKNOWN
-  ) {
+  } else if (rarity === "N/A" || rarity === RarityTypes.UNKNOWN) {
     return "HOLY S**T!! WTF IS THAT?!";
+  } else if (rarity === RarityTypes.LEGENDARY) {
+    return "D$%#MN THATS LEGENDARY";
   }
 };
 
@@ -81,7 +85,7 @@ const RarityTool = () => {
   function fetchData(url: string) {
     if (!url || !bots[url]) {
       if (url !== "") {
-        showErrorToast();
+        showToast(1);
       }
       return;
     }
@@ -90,15 +94,17 @@ const RarityTool = () => {
     axios
       .get(bots[url].link)
       .then((response) => {
+        console.log("response", response.data);
         setBot(response.data);
         setLoading(false);
       })
       .catch((err) => console.log(err));
   }
 
-  const showErrorToast = () => {
-    AppToaster.show({
-      message: (
+  const showToast = (num: number) => {
+    let message;
+    if (num === 1) {
+      message = (
         <div className="text-3xl uppercase flex items-center rarity-tool__error-toast">
           <div>
             <img
@@ -112,17 +118,30 @@ const RarityTool = () => {
             Quit wasting my time!!"
           </div>
         </div>
-      ),
+      );
+    } else if (num === 2) {
+      message = <div>sdf</div>;
+    }
+    AppToaster.show({
+      message,
     });
   };
 
-  const renderIcon = (rarity: string) => {
+  const renderIcon = (
+    rarity: string,
+    category: string,
+    traitName: string | undefined
+  ) => {
+    const isNoneEquipment = category === "Equipment" && traitName === "none";
+    const isNoneDamage = category === "Damage" && traitName === "none";
     const classname = cx("rarity-tool__icon", {
-      "rarity-tool__icon--common": rarity === RarityTypes.COMMON,
+      "rarity-tool__icon--common":
+        rarity === RarityTypes.COMMON || isNoneEquipment,
       "rarity-tool__icon--rare": rarity === RarityTypes.RARE,
-      "rarity-tool__icon--epic": rarity === RarityTypes.EPIC,
+      "rarity-tool__icon--epic": rarity === RarityTypes.EPIC || isNoneDamage,
       "rarity-tool__icon--legendary": rarity === RarityTypes.LEGENDARY,
-      "rarity-tool__icon--unknown": rarity === RarityTypes.UNKNOWN,
+      "rarity-tool__icon--unknown":
+        rarity === RarityTypes.UNKNOWN || rarity === "N/A",
     });
     return <div className={classname} />;
   };
@@ -152,7 +171,7 @@ const RarityTool = () => {
     }
     return (
       <div>
-        {categories.map((category) => {
+        {categories.map((category, index) => {
           const matchingCategory = find(
             bot?.attributes,
             (trait) => trait.trait_type === category
@@ -160,7 +179,7 @@ const RarityTool = () => {
           const traitName = matchingCategory?.value;
           let rarity = get(rarityHashMap, [`${traitName}`], "N/A");
           return (
-            <div className="tracking-widest flex uppercase">
+            <div key={index} className="tracking-widest flex uppercase">
               <div className="text-white text-center rarity-tool__trait rarity-tool__trait--background rarity-tool__trait--bordered">
                 {category === "Backgrounds" ? "Background" : category}
               </div>
@@ -169,7 +188,7 @@ const RarityTool = () => {
               </div>
               <div className="bg-white text-black text-center rarity-tool__trait rarity-tool__trait--bordered">
                 {bot ? getRarityString(rarity, category, traitName) : "?"}
-                {bot && renderIcon(rarity)}
+                {bot && renderIcon(rarity, category, traitName)}
               </div>
             </div>
           );
@@ -224,18 +243,26 @@ const RarityTool = () => {
                   )}
                 </>
               )}
-
-              <div className="rarity-tool__bot-name mt-3 uppercase text-center border-black border-4 border-solid bg-white flex items-center justify-center">
-                {bot?.name}
-              </div>
+              {bot && (
+                <div className="rarity- tool__bot-name mt-3 uppercase text-center border-black border-4 border-solid bg-white flex items-center justify-center">
+                  {bot?.name}
+                </div>
+              )}
             </div>
             <div className="ml-6">
               <div className="mb-1 tracking-widest">UNIT #</div>
-              <input
-                className="rarity-tool__search w-full mb-4"
-                type="text"
-                onChange={(e) => setQuery(e.currentTarget.value)}
-              />
+              <div className="relative">
+                <Icon
+                  className="absolute rarity-tool__search-icon"
+                  icon="search"
+                  size={IconSize.STANDARD}
+                />
+                <input
+                  className="rarity-tool__search w-full mb-4 pl-5"
+                  type="text"
+                  onChange={(e) => setQuery(e.currentTarget.value)}
+                />
+              </div>
               {renderTraits(bot)}
             </div>
           </div>
