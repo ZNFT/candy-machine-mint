3982670fc7226f496e65e688d6bc8a79f5752f3d
Eric Kao <eric.kao@enjoy.com>
2021-10-17 02:21:13 -0700

    [ADD] Use attribute new json

--- src/utils/getBotsFromHash.ts
+++ src/utils/getBotsFromHash.ts
@@ -1,35 +1,42 @@
-import { BotType } from "../pages/RarityTool"
-import { botsJson } from "./bots"
 import { wave2Hash, wave3Hash } from "./paired-bots"
-import { wave3 } from "./updatedWave3"
-import { wave4 } from "./wave4"
+import { wave1 } from "../bots/wave1"
+import { wave3 } from "../bots/wave3"
+import { wave2 } from "../bots/wave2"
+import { wave4 } from "../bots/wave4"
+
+export type BotDataType = {
+  gen: string,
+  img: string,
+  attributes: {
+    trait_type: string,
+    value: string | number
+  }[]
+  name: string
+}
+
+export type BotsDataType = {
+  [key: string]: BotDataType
+}
 
 export const getBotsFromHash = (num: string) => {
-  const botsArray: BotType[] = []
-  if(botsJson[num]){
-    botsArray.push(botsJson[num])
-  } else if(wave3[num]){
-    botsArray.push(wave3[num])
-  } else if(wave4[num]){
-    botsArray.push(wave4[num])
+  const arr = []
+  if(wave1[num]){
+    arr.push(wave1[num])
+  }
+  else if(wave2[num]){
+    arr.push(wave2[num])
+  }
+  else if(wave3[num]){
+    arr.push(wave3[num])
+  }
+  else if(wave4[num]){
+    arr.push(wave4[num])
   }
 
   if(wave2Hash[num]){
-    botsArray.push(wave3[wave2Hash[num]])
+    arr.push(wave3[wave2Hash[num]])
   } else if(wave3Hash[num]){
-    botsArray.push(botsJson[wave3Hash[num]])
+    arr.push(wave2[wave3Hash[num]])
   }
-  return botsArray;
-}
-
-export const getWaveName = (collection: string) => {
-  if(collection === 'Gen 1 heads'){
-    return 'Wave 1'
-  } else if(collection === 'Gen 2 heads'){
-    return 'Wave 2'
-  } else if(collection === 'Gen 3 heads'){
-    return 'Wave 3'
-  } else if(collection === 'Gen 4 heads'){
-    return 'Wave 4'
-  }
+  return arr;
 }
